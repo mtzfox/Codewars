c09b5528669d357529b35cf6daf792bb4e9c135f
Mike Carlson <21209421+redfoxseattle@users.noreply.github.com>
2021-11-15 19:57:56 -0800

    codewars multiplication table answer

---
+++ multiplicationTable.js
@@ -0,0 +1,13 @@
+multiplicationTable = function (size) {
+	// array for all returned num arrays
+	const tables = [];
+	// counts the first few number keys, then slices the everything but the first number (0)
+	let first = [...Array(size + 1).keys()].slice(1, size.length);
+
+	// starting with 2nd item until equal to last number index,
+	// pushes multiplication of first array that is mapped to be multiplied by the index (*2, *3, etc)
+	for (let i = 1; i <= size; i++) {
+		tables.push([...first].map((num) => num * i));
+	}
+	return tables;
+};
