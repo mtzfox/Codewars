080f2ce0d6f6f69d26b038fd96579a12e29c4902
Mike Carlson <21209421+redfoxseattle@users.noreply.github.com>
2021-11-20 05:05:55 -0800

    codewars countIPAddress finish

--- countIPAddresses.js
+++ countIPAddresses.js
@@ -1,38 +1,19 @@
-// It's very close, I think I know the issue.
-/* 
-Rather than simply totaling the amount (which could be done with reduce(), anyway) I need push each value to an array and then compare the two arrays. Because each point of the array has different values, so if the 3rd element is 1 different it's 255pts different than if the 4th element is 1 different. I need to identify which number has changed and then evaluate the difference of that value.
-*/
-
-const binToDec = (bit) => parseInt(bit, 2);
-const decToBin = (dec) => Number(dec.toString(2));
-
 function ipsBetween(start, end) {
-	console.log(start, end);
+	let ip1 = start.split('.'),
+		ip2 = end.split('.'),
+		difference = [],
+		host = 0,
+		network = 0,
+		octetClass = 3;
 
-	let ip1 = start.split('.');
-	let ip2 = end.split('.');
-	let ip1total = 0;
-	let ip2total = 0;
-	let difference = 0;
-	//console.log(decToBin(10))
 	for (let i in ip1) {
-		ip1total += decToBin(Number(ip1[i]));
+		ip2[i] != ip1[i] ? (difference[i] = ip2[i] - ip1[i]) : (difference[i] = 0);
 	}
-	//console.log(ip2)
-	for (let i in ip2) {
-		ip2total += decToBin(Number(ip2[i]));
+	host = difference.pop();
 
-		// if (i.valueOf() == 3) {
-		//   //console.log("I'm 3")
-		// }
+	for (let i = 0; i < difference.length; i++) {
+		network += difference[i] * Math.pow(256, octetClass);
+		octetClass--;
 	}
-	console.log(ip2total - ip1total);
-	//difference = Number(binToDec(ip2total) - binToDec(ip1total))
-	//difference = (binToDec(ip2total) - binToDec(ip1total));
-	//return binToDec(difference)
+	return network + host;
 }
-
-//ipToBin(ip)
-//decToBin(20)
-ipsBetween('10.0.0.0', '10.0.0.50');
-ipsBetween('10.0.0.0', '10.0.1.0');
